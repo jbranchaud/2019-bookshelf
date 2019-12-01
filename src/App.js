540d84341b2a654eb6888448dd34014227025978
jbranchaud <jbranchaud@gmail.com>
2019-12-01 12:41:01 -0600

    Initial effort to display images with flexbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import behindTheBeautifulForevers from "./images/book-photos/behind-the-beautiful-forevers.jpeg";
+import homegoing from "./images/book-photos/homegoing.jpeg";
+import theCityAndTheCity from "./images/book-photos/the-city-and-the-city.jpeg";
+import rework from "./images/book-photos/rework.jpeg";
+
+function BookPhoto({ photo, alt }) {
+  return (
+    <div className="book-photo">
+      <img src={photo} alt={alt} />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Books - 2019</p>
       </header>
+      <div className="books-container">
+        <BookPhoto photo={behindTheBeautifulForevers} alt="Behind The Beautiful Forevers" />
+        <BookPhoto photo={homegoing} alt="Homegoing" />
+        <BookPhoto photo={theCityAndTheCity} alt="The City & the City" />
+        <BookPhoto photo={rework} alt="Rework" />
+      </div>
     </div>
   );
 }
